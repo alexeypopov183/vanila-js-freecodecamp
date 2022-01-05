acb0b00fd5ed7ed45bccda467738d97be5e099b9
Alexey Popov <alexeypopov183@gmail.com>
2022-01-06 00:16:58 +0800

    feat: add function hex.js

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+let getRandomItem = () => {
+  return Math.floor(Math.random() * hex.length)
+}
+
+btn.addEventListener('click', () => {
+  let colorHex = '#'
+  for(let i = 0; i < 6; i++) {
+    colorHex += hex[getRandomItem()];
+  }
+  document.body.style.backgroundColor = colorHex;
+  color.textContent = colorHex;
+})
